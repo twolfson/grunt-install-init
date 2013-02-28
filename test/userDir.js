2f5a0c32a5f5f7fb7991fa7f412e1e1fd13002f3
Todd Wolfson <todd@twolfson.com>
2013-02-28 04:16:26 -0800

    Resolving userDir (yay)

--- test/userDir.js
+++ test/userDir.js
@@ -3,7 +3,21 @@ module.exports = function (grunt) {
 
   // If there is no grunt.file.userDir, hackishly grab it from grunt-init
   if (!userDir) {
-    var gruntInitPath = require.resolve('grunt-init');
-    console.log(gruntInitPath);
+    // Cannot resolve since there is no `main` in grunt-init
+    // var gruntInitPath = require.resolve('grunt-init');
+
+    // We could use https://github.com/gruntjs/grunt/blob/master/lib/grunt/task.js#L369-L371
+    // but that is not easily exposed either
+
+    // Hardcoding lookup as grunt could do.
+    var initHelpersPath = __dirname + '/../node_modules/grunt-init/tasks/lib/helpers.js';
+
+    // Load in helpers and save userDir
+    var initHelpers = require(initHelpersPath).init(grunt);
+    userDir = initHelpers.userDir;
+    console.log(userDir);
   }
+
+  // Return userDir
+  return userDir;
 };
